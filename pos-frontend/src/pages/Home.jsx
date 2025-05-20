349b3471b3582c140697346fb24cfd9270399d16
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-05-21 01:20:29 +0530

    Add bottom navigation and add styles

--- pos-frontend/src/pages/Home.jsx
+++ pos-frontend/src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import BottomNav from "../components/shared/BottomNav.jsx";
 
 function Home() {
     return (
-        <div>Home</div>
+        <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
+            { /* Left Div */ }
+            <div className="flex-[3] bg-[#1a1a1a] ">
+
+            </div>
+
+            { /* Right Div */ }
+
+            <div className="flex-[2] bg-[#1a1a1a] ">
+            </div>
+            <BottomNav />
+
+        </section>
     );
 }
 
