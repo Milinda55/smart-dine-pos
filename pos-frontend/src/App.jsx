38c00fc89ee5d4f84e7d8708d76120f1f96d71db
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-05-20 23:37:48 +0530

    Create the routing paths

--- pos-frontend/src/App.jsx
+++ pos-frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
 import './index.css'
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { Home, Auth, Orders } from './pages'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl">Hello POS</h1>
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/auth" element={<Auth />} />
+              <Route path="/orders" element={<Orders />} />
+          </Routes>
+      </Router>
     </>
   )
 }
