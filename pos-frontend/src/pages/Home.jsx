7cf96075269d4a1c1b9a054ec42e33f7047bf69f
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-05-22 18:52:19 +0530

    Add data and time to the greeting

--- pos-frontend/src/pages/Home.jsx
+++ pos-frontend/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import BottomNav from "../components/shared/BottomNav.jsx";
+import Greetings from "../components/home/Greetings.jsx";
 
 function Home() {
     return (
         <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
             { /* Left Div */ }
-            <div className="flex-[3] bg-[#1a1a1a] ">
-
+            <div className="flex-[3]">
+                <Greetings />
             </div>
 
             { /* Right Div */ }
