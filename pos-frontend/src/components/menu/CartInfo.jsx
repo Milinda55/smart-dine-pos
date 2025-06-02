8c3c910e4c2f3d8644820001cbe28c92ff886963
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-06-03 02:01:02 +0530

    Create add and remove cart items

--- pos-frontend/src/components/menu/CartInfo.jsx
+++ pos-frontend/src/components/menu/CartInfo.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {RiDeleteBin2Fill} from "react-icons/ri";
 import {FaNotesMedical} from "react-icons/fa";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {removeItem} from "../../redux/slices/cartSlice.js";
 
 function CartInfo() {
 
     const cartData = useSelector((state) => state.cart)
+    const dispatch = useDispatch();
+
+    const handleRemove = (id) => {
+        dispatch(removeItem(id))
+    }
 
     return (
         <div className="px-4 py-2">
@@ -13,33 +19,18 @@ function CartInfo() {
             <div className="mt-4 overflow-y-scroll h-[200px] scrollbar-hide">
                 {cartData.length === 0 ? (<p className="text-[#ababab] text-lg flex justify-center items-center h-[200px]">Your cart is empty</p>) : cartData.map((item) => {
 
-
-
                     return (
                         <div className="bg-[#1f1f1f] rounded-lg px-4 mb-2 py-4">
                             <div className="flex items-center justify-between">
-                                <h1 className="text-[#ababab] font-semibold tracking-wide text-md">Chicken rice</h1>
-                                <p className="text-[#ababab] font-semibold">x2</p>
+                                <h1 className="text-[#ababab] font-semibold tracking-wide text-md">{item.name}</h1>
+                                <p className="text-[#ababab] font-semibold">x{item.quantity}</p>
                             </div>
                             <div className="flex items-center justify-between mt-3">
                                 <div className="flex items-center gap-3">
-                                    <RiDeleteBin2Fill className="text-[#ababab] cursor-pointer" size={20}/>
+                                    <RiDeleteBin2Fill onClick={() => handleRemove(item.id)} className="text-[#ababab] cursor-pointer" size={20}/>
                                     <FaNotesMedical className="text-[#ababab] cursor-pointer" size={20}/>
                                 </div>
-                                <p className="text-[#f5f5f5] text-md font-bold">Rs.250</p>
-                            </div>
-                            <div className="bg-[#1f1f1f] rounded-lg px-4 mb-2 py-4">
-                                <div className="flex items-center justify-between">
-                                    <h1 className="text-[#ababab] font-semibold tracking-wide text-md">Chicken rice</h1>
-                                    <p className="text-[#ababab] font-semibold">x2</p>
-                                </div>
-                                <div className="flex items-center justify-between mt-3">
-                                    <div className="flex items-center gap-3">
-                                        <RiDeleteBin2Fill className="text-[#ababab] cursor-pointer" size={20}/>
-                                        <FaNotesMedical className="text-[#ababab] cursor-pointer" size={20}/>
-                                    </div>
-                                    <p className="text-[#f5f5f5] text-md font-bold">Rs.250</p>
-                                </div>
+                                <p className="text-[#f5f5f5] text-md font-bold">Rs.{item.price}</p>
                             </div>
                         </div>)
                 })}
--- pos-frontend/src/components/menu/MenuContainer.jsx
+++ pos-frontend/src/components/menu/MenuContainer.jsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import { menus } from "../../constants/index.js";
 import { GrRadialSelected } from "react-icons/gr";
 import { FaShoppingCart } from "react-icons/fa";
+import {useDispatch} from "react-redux";
+import {addItems} from "../../redux/slices/cartSlice.js";
 
 function MenuContainer() {
     const [selected, setSelected] = useState(menus[0]);
-    const [itemCounts, setItemCounts] = useState({});
+    const [itemCounts, setItemCounts] = useState(0);
+    const dispatch = useDispatch();
 
     const increment = (id) => {
         setItemCounts((prev) => {
@@ -23,24 +26,34 @@ function MenuContainer() {
         });
     }
 
+    const handleAddToCart = (item) => {
+        if (itemCounts === 0) return alert("Please add some items to cart");
+
+        const {name, price} = item;
+        const newObj = { id: new Date(), name, pricePerQuantity: price, quantity: itemCounts[item.id], price: itemCounts[item.id] * price };
+
+        dispatch(addItems(newObj));
+        setItemCounts(0);
+    }
+
     return (
         <>
             <div className='grid grid-cols-4 px-10 py-2 gap-4 w-[100%]'>
                 {
-                    menus.map((menu) => {
+                    menus.map((item) => {
                         return (
-                            <div key={menu.id}
+                            <div key={item.id}
                                  className="flex flex-col items-start justify-between p-4 rounded-lg h-[100px] cursor-pointer"
-                                 style={{ backgroundColor: menu.bgColor }}
+                                 style={{ backgroundColor: item.bgColor }}
                                  onClick={() => {
-                                     setSelected(menu);
+                                     setSelected(item);
                                  }}
                             >
                                 <div className="flex items-center justify-between w-full">
-                                    <h1 className="text-[#f5f5f5] text-lg font-semibold">{menu.icon} {menu.name}</h1>
-                                    {selected.id === menu.id && <GrRadialSelected className="text-white size={20}" />}
+                                    <h1 className="text-[#f5f5f5] text-lg font-semibold">{item.icon} {item.name}</h1>
+                                    {selected.id === item.id && <GrRadialSelected className="text-white size={20}" />}
                                 </div>
-                                <p className="text-[#ababab] text-sm font-semibold">{menu.items.length} Items</p>
+                                <p className="text-[#ababab] text-sm font-semibold">{item.items.length} Items</p>
                             </div>
                         )
                     })
@@ -49,25 +62,25 @@ function MenuContainer() {
             <hr className="border-[#333] border-t-2 mt-4" />
             <div className='grid grid-cols-4 px-10 py-4 gap-4 w-[100%]'>
                 {
-                    selected.items.map((menu) => {
-                        const count = itemCounts[menu.id] || 0;
+                    selected.items.map((item) => {
+                        const count = itemCounts[item.id] || 0;
 
                         return (
-                            <div key={menu.id}
+                            <div key={item.id}
                                  className="flex flex-col items-start justify-between p-4 rounded-lg h-[120px] cursor-pointer bg-[#1a1a1a] hover:bg-[#2c2c2c]"
                             >
                                 <div className="flex items-start justify-between w-full">
-                                    <h1 className="text-[#f5f5f5] text-lg font-semibold">{menu.name}</h1>
-                                    <button className="bg-[#2e4a40] text-[#02ca3a] p-2 rounded-lg">
+                                    <h1 className="text-[#f5f5f5] text-lg font-semibold">{item.name}</h1>
+                                    <button onClick={() => handleAddToCart(item)} className="bg-[#2e4a40] text-[#02ca3a] p-2 rounded-lg">
                                         <FaShoppingCart size={15} />
                                     </button>
                                 </div>
                                 <div className="flex items-center justify-between w-full">
-                                    <p className="text-white text-lg font-bold">Rs.{menu.price}</p>
+                                    <p className="text-white text-lg font-bold">Rs.{item.price}</p>
                                     <div className="flex items-center justify-between bg-[#1f1f1f] rounded-lg px-2 py-1 gap-6">
-                                        <button onClick={() => decrement(menu.id)} className="text-yellow-500 text-xl cursor-pointer">&minus;</button>
+                                        <button onClick={() => decrement(item.id)} className="text-yellow-500 text-xl cursor-pointer">&minus;</button>
                                         <span className="text-white">{count}</span>
-                                        <button onClick={() => increment(menu.id)} className="text-yellow-500 text-xl cursor-pointer">&#43;</button>
+                                        <button onClick={() => increment(item.id)} className="text-yellow-500 text-xl cursor-pointer">&#43;</button>
                                     </div>
                                 </div>
                             </div>
