3d790050a5ba1541e0eb28d19f80811246bf0214
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-06-02 17:15:31 +0530

    Update table card

--- pos-frontend/src/components/shared/BottomNav.jsx
+++ pos-frontend/src/components/shared/BottomNav.jsx
@@ -5,13 +5,19 @@ import {CiCircleMore} from "react-icons/ci";
 import {BiSolidDish} from "react-icons/bi";
 import {useLocation, useNavigate} from "react-router-dom";
 import Modal from "./Modal.jsx";
+import {useDispatch} from "react-redux";
+import {setCustomer} from "../../redux/slices/customerSlice.js";
 
 function BottomNav() {
 
     const navigate = useNavigate();
     const location = useLocation();
+    const dispatch = useDispatch();
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [guestCount, setGuestCount] = useState(0);
+    const [name, setName] = useState();
+    const [phone, setPhone] = useState();
+
     const openModal = () => setIsModalOpen(true);
     const closeModal = () => setIsModalOpen(false);
 
@@ -26,6 +32,12 @@ function BottomNav() {
     }
 
     const isActive = (path) => location.pathname === path;
+
+    const handleCreateOrder = () => {
+        // send the data to the store
+        dispatch(setCustomer({name, phone, guests: guestCount}));
+        navigate("/tables");
+    }
 
     return (
         <div className="fixed bottom-0 left-0 right-0 bg-[#262626] p-2 h-16 flex justify-around">
@@ -57,14 +69,14 @@ function BottomNav() {
                 <div>
                     <label className="block text-[#ababab] mb-2 text-sm font-medium">Customer Name</label>
                     <div className="flex items-center bg-[#1f1f1f] rounded-lg px-4 p-3">
-                        <input type="text" className="bg-transparent flex-1 text-white focus:outline-none"
+                        <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="bg-transparent flex-1 text-white focus:outline-none"
                                placeholder="Enter Customer Name" name="" id=""/>
                     </div>
                 </div>
                 <div>
                     <label className="block text-[#ababab] mb-2 mt-3 text-sm font-medium">Customer Phone</label>
                     <div className="flex items-center bg-[#1f1f1f] rounded-lg px-4 p-3">
-                        <input type="number" className="bg-transparent flex-1 text-white focus:outline-none"
+                        <input value={phone} onChange={(e) => setPhone(e.target.value)} type="number" className="bg-transparent flex-1 text-white focus:outline-none"
                                placeholder="Enter Customer Phone" name="" id=""/>
                     </div>
                 </div>
@@ -75,7 +87,7 @@ function BottomNav() {
                         <span className="text-white">{guestCount} Person</span>
                         <button onClick={increment} className="text-yellow-500 text-2xl cursor-pointer">&#43;</button>
                     </div>
-                    <button onClick={() => navigate("/tables")}
+                    <button onClick={handleCreateOrder}
                             className="bg-[#f68100] text-[#f5f5f5] rounded-lg py-3 w-full mt-8 text-center font-semibold hover:bg-[#f6b100] cursor-pointer">
                         Create Order
                     </button>
--- pos-frontend/src/components/tables/TableCard.jsx
+++ pos-frontend/src/components/tables/TableCard.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import {FaCheckDouble} from "react-icons/fa";
 import {getBgColor} from "../../utils/index.js";
 import {useNavigate} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {updateTable} from "../../redux/slices/customerSlice.js";
 
 function TableCard({keys, name, status, initials, seats}) {
 
+    const dispatch = useDispatch();
     const navigate = useNavigate();
-    const handleClick = () => {
+    const handleClick = (name) => {
         if (status === "Booked") return;
+        dispatch(updateTable({tableNo: name}))
         navigate('/menu');
     }
 
     return (
-        <div onClick={handleClick} key={keys} className="w-[300px] bg-[#262626] hover:bg-[#2c2c2c] p-4 rounded-lg mb-4 cursor-pointer">
+        <div onClick={() => handleClick(name)} key={keys} className="w-[300px] bg-[#262626] hover:bg-[#2c2c2c] p-4 rounded-lg mb-4 cursor-pointer">
             <div className="flex items-center justify-between px-1">
                 <h1 className="text-[#f5f5f5] text-xl font-semibold">{name}</h1>
                 <p className={`${status === "Booked" ? "text-green-600 bg-[#2e4a40]" : "bg-[#664a04] text-white"} px-2 py-1 rounded-lg`}>{status}</p>
