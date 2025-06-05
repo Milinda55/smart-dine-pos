e08b5430339dbcc1d1a3ec142ddeef569de8527a
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-06-06 04:37:23 +0530

    Add table parameters and add lombok

---
+++ pos-backend/src/main/java/com/milinda/pos/model/Table.java
@@ -0,0 +1,27 @@
+package com.milinda.pos.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "tables")
+public class Table {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private Integer tableNo;
+
+    private String status = "Available";
+
+    @DBRef
+    private Order currentOrder;
+
+    public enum TableStatus {
+        AVAILABLE, OCCUPIED, RESERVED, CLEANING
+    }
+}
