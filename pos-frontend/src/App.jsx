163c5d791224c76578ea89f379ac71f263a8b0cd
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-06-06 16:31:33 +0530

    Update app.jsx to make header hide in auth page

--- pos-frontend/src/App.jsx
+++ pos-frontend/src/App.jsx
@@ -1,25 +1,34 @@
 import './App.css'
 import './index.css'
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, useLocation} from "react-router-dom";
 import {Home, Auth, Orders, Tables, Menu} from './pages'
 import Header from "./components/shared/Header.jsx";
 
+function Layout() {
+    const location = useLocation();
+    const hideHeaderRoutes = ["/auth"];
+
+    return (
+        <>
+            { !hideHeaderRoutes.includes(location.pathname) && <Header />}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/auth" element={<Auth />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/tables" element={<Tables />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="*" element={<div>Not Found</div>} />
+            </Routes>
+        </>
+    )
+}
+
 function App() {
-  return (
-    <>
-      <Router>
-          <Header />
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/auth" element={<Auth />} />
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/tables" element={<Tables />} />
-              <Route path="/menu" element={<Menu />} />
-              <Route path="*" element={<div>Not Found</div>} />
-          </Routes>
-      </Router>
-    </>
-  )
+    return (
+        <Router>
+            <Layout />
+        </Router>
+    )
 }
 
 export default App
