977ae2b29cdd112665659f37784c820f084fc59d
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-06-13 16:25:45 +0530

    Set protected routes and set redirects

--- pos-frontend/src/App.jsx
+++ pos-frontend/src/App.jsx
@@ -1,26 +1,55 @@
 import './App.css'
 import './index.css'
-import {BrowserRouter as Router, Route, Routes, useLocation} from "react-router-dom";
+import {BrowserRouter as Router, Navigate, Route, Routes, useLocation} from "react-router-dom";
 import {Home, Auth, Orders, Tables, Menu} from './pages'
 import Header from "./components/shared/Header.jsx";
+import {useSelector} from "react-redux";
 
 function Layout() {
     const location = useLocation();
     const hideHeaderRoutes = ["/auth"];
+    const { isAuth } = useSelector(state => state.user)
 
     return (
         <>
             { !hideHeaderRoutes.includes(location.pathname) && <Header />}
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/auth" element={<Auth />} />
-                <Route path="/orders" element={<Orders />} />
-                <Route path="/tables" element={<Tables />} />
-                <Route path="/menu" element={<Menu />} />
+                <Route path="/" element={
+                    <ProtectedRoutes>
+                        <Home />
+                    </ProtectedRoutes>
+                } />
+                
+                <Route path="/auth" element={isAuth ? <Navigate to="/" /> : <Auth /> } />
+
+                <Route path="/orders" element={
+                    <ProtectedRoutes>
+                        <Orders />
+                    </ProtectedRoutes>
+                } />
+                <Route path="/tables" element={
+                    <ProtectedRoutes>
+                        <Tables />
+                    </ProtectedRoutes>
+                } />
+                <Route path="/menu" element={
+                    <ProtectedRoutes>
+                        <Menu />
+                    </ProtectedRoutes>
+                } />
                 <Route path="*" element={<div>Not Found</div>} />
             </Routes>
         </>
     )
+}
+
+function ProtectedRoutes({children}) {
+    const { isAuth } = useSelector(state => state.user )
+    if (!isAuth) {
+        return <Navigate to={"/auth"} />
+    }
+
+    return children;
 }
 
 function App() {
